f7602eb25d8ca46e73aa0abb818869741ea83f42
Nelia <nelia.courtais@live.fr>
2023-10-27 14:59:30 +0200

    little changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [playlists, setPlaylists] = useState([]);
   const getPlaylist = async () => {
     const token =
-      "BQCSe5gAhm54RgI7FXlopTEv-mjF4gRN1nnYJDJMUQ17Kw7uKXwr-c2aM6_lFaaLaPRYCmA4aCoQUuYi0XgCRNj3yqST0MMiif1s28qdha-qlTbh-8M";
+      "BQDyJW32z5870eVNnsy9Bsh6OMNKPZykID8hBDPXqk8n55BAAjmP8ht9wgFJU2xeuXaJkQ4hs_vqDZfyIqCXHdg2Nke4Cs0dfjLv1bhRy02S49VxM3k";
 
     const reponse = await axios.get(
       "https://api.spotify.com/v1/browse/categories/0JQ5DAqbMKFKLfwjuJMoNC/playlists",
