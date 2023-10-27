982d20caca79ec79b8e50eb6085072a2eb2bd1d9
Benoit-Hayet <hayetbenoit@gmail.com>
2023-10-27 17:03:20 +0100

    first push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,50 @@ import "./style/App.scss";
 
 function App() {
   const [playlists, setPlaylists] = useState([]);
-  const getPlaylist = async () => {
-    const token =
-      "BQCVAf7-rWDL5mNL50pB_ofCJqymAPYdSiL2Fh5YvHr4yOvpyocKvKKuhqIqlXcpg3mWft4hcQ6NvLe5Fjb52FniPwhSqhvGTQ_v-ymLnBLr6158ZEg";
 
-    const reponse = await axios.get(
+  // Fonction pour effectuer la première requête
+  const getFirstPlaylist = async () => {
+    const token =
+      "BQC8WIDZFiK04LhL88V_cxRb3-cCRtjq3cXBbBymyGxLMcdH3Hw6coc92H4pRP0xIJFJqs-aS2i_rwgOn3TIHZLE2AwLge-lAlCRMiJ26KAD99lJpZA"; // Remplacez par votre token Spotify
+
+    const response = await axios.get(
+      "https://api.spotify.com/v1/search?q=chill&type=playlist",
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    setPlaylists(response.data.playlists.items);
+  };
+
+  // Fonction pour effectuer la deuxième requête
+  const getSecondPlaylist = async () => {
+    const token =
+      "BQC8WIDZFiK04LhL88V_cxRb3-cCRtjq3cXBbBymyGxLMcdH3Hw6coc92H4pRP0xIJFJqs-aS2i_rwgOn3TIHZLE2AwLge-lAlCRMiJ26KAD99lJpZA"; // Remplacez par votre token Spotify
+
+    const response = await axios.get(
       "https://api.spotify.com/v1/browse/categories/0JQ5DAqbMKFKLfwjuJMoNC/playlists",
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
-    setPlaylists(reponse.data.playlists.items);
+    setPlaylists(response.data.playlists.items);
   };
 
   return (
     <div className="container">
       <div className="btn-container">
-        <button className="btn-generation" type="button" onClick={getPlaylist}>
-          Générer des playlists !
+        <button
+          className="btn-generation"
+          type="button"
+          onClick={getFirstPlaylist}
+        >
+          Obtenir la première playlist
+        </button>
+
+        <button
+          className="btn-generation"
+          type="button"
+          onClick={getSecondPlaylist}
+        >
+          Obtenir la deuxième playlist
         </button>
       </div>
 
