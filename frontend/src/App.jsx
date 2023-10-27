c7d34046a79fe6dd4a935a7f79b0bc30a69f07aa
Hepsox <marie.delaire@outlook.com>
2023-10-27 23:03:39 +0200

    debut fonctionnalité dynamisation de la carte

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,30 @@ import axios from "axios";
 import { useState } from "react";
 import Cards from "./components/Cards";
 import "./style/App.scss";
-import Direction from "./models/Moods";
+import Moods from "./models/Moods";
 
 function App() {
   const [playlists, setPlaylists] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const token =
+    "BQDt9d4OkBmJov8MO2eH3rR5JrdVYaimi3EsSsGCM9f3-CnCv0NqxmjQccbR_vHfP0xNuUpBlKn_0ICn7NL8V0HZYrj6Bd4SQg9oNpZ2y9My0FD1eWM";
 
   const getPlaylist = async () => {
-    const token =
-      "BQBULDxixWbgGC733VNdzdgjs5px4Z-0Bi_TgNwgmnpqUPwhKC06bP25HhImG8-cCxvvCeerHiRCRpEcr8PxYfhwXlddQL8O_v5rhnM0gjNNxMK__YA";
-
     const reponse = await axios.get(
-      `https://api.spotify.com/v1/browse/categories/${Direction.Chill}/playlists`,
+      `https://api.spotify.com/v1/browse/categories/${Moods.Chill}/playlists`,
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
     setPlaylists(reponse.data.playlists.items);
+  };
+
+  const getTrackList = async () => {
+    const reponseTrack = await axios.get(
+      `https://api.spotify.com/v1/playlists/37i9dQZF1DX7aUUBCKwo4Y/tracks`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    setTracks(reponseTrack.data.items);
   };
 
   return (
@@ -24,6 +33,9 @@ function App() {
       <div className="btn-container">
         <button className="btn-generation" type="button" onClick={getPlaylist}>
           Playlist Happy !
+        </button>
+        <button className="btn-generation" type="button" onClick={getTrackList}>
+          La tracklist
         </button>
       </div>
 
@@ -35,8 +47,15 @@ function App() {
               imgCover={element.images[0].url}
               playlistCategory={element.description}
               key={element.id}
+              playlistId={element.id}
             />
           );
+        })}
+      </div>
+
+      <div>
+        {tracks.map((track) => {
+          return <Cards infoTrack={track.track.album.images[1].url} />;
         })}
       </div>
     </div>
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
 
-function Cards({ playlistName, imgCover, playlistCategory }) {
+function Cards({
+  playlistName,
+  imgCover,
+  playlistCategory,
+  playlistId,
+  infoTrack,
+}) {
   return (
     <div className="card">
       <img className="img-fluid" src={imgCover} alt="cover" />
       <h1>{playlistName}</h1>
       <p>{playlistCategory}</p>
+      <p>{playlistId}</p>
+      <img src={infoTrack} alt="cover" />
     </div>
   );
 }
@@ -14,6 +22,8 @@ Cards.propTypes = {
   playlistName: PropTypes.string.isRequired,
   imgCover: PropTypes.string.isRequired,
   playlistCategory: PropTypes.string.isRequired,
+  playlistId: PropTypes.string.isRequired,
+  infoTrack: PropTypes.string.isRequired,
 };
 
 export default Cards;
