c125e6cc0e3093272c97ede94b0a406e6ea9ad67
Hepsox <marie.delaire@outlook.com>
2023-11-06 09:45:45 +0100

    fix bug

--- frontend/src/components/Tracks.jsx
+++ frontend/src/components/Tracks.jsx
@@ -4,7 +4,7 @@ function Tracks({ nameTrack, imgCoverTrack }) {
   return (
     <div>
       <h2>{nameTrack}</h2>
-      <img className="img-fluid" src={imgCoverTrack} alt="img" />
+      <img className="img-fluid" src={imgCoverTrack} alt="imgalt" />
     </div>
   );
 }
