7a81d358a9a5519674e373e3e26258aba34dc98c
Hepsox <marie.delaire@outlook.com>
2023-11-06 10:00:50 +0100

    ajout des nouveaux styles

--- frontend/src/pages/TemplateHome.jsx
+++ frontend/src/pages/TemplateHome.jsx
@@ -5,9 +5,11 @@ function TemplateHome() {
   return (
     <div className="container">
       <div className="mood-playlist">
-        <Link to="/playlist/acoustic">Acoustic</Link>
-        <Link to="/playlist/afrobeat">Afrobeat</Link>
-        <Link to="/playlist/anime">Animé</Link>
+        <Link to="/playlist/country">Soirée au camping Les flots Bleus</Link>
+        <Link to="/playlist/kids">Anniversaire du petit Antoine</Link>
+        <Link to="/playlist/reggaeton">Tu veux pécho ?</Link>
+        <Link to="/playlist/sad">Dimanche soir de bonne humeur</Link>
+        <Link to="/playlist/death-metal">Pour se détendre</Link>
       </div>
     </div>
   );
