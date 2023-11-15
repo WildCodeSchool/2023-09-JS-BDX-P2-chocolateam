8628893d0f0c3256042ce254a053882375ab9126
Nelia <nelia.courtais@live.fr>
2023-11-15 17:27:49 +0100

    change style track

--- frontend/src/pages/TemplateTracklist.jsx
+++ frontend/src/pages/TemplateTracklist.jsx
@@ -8,19 +8,22 @@ function TemplateTracklist() {
 
   return (
     <div className="resultats-tracks">
-      <NavBis />
-      <Redirection />
-      {tracklists.map((element) => (
-        <div key={element.track.id}>
+      <div className="navContainer">
+        <NavBis />
+        <Redirection />
+      </div>
+      <div className="trackContainer">
+        {tracklists.map((element) => (
           <CardTrack
+            key={element.track.id}
             trackArtistName={element.track.artists[0].name}
             trackName={element.track.name}
             trackImgCover={element.track.album.images[0].url}
-            trackPreview={element.track?.preview_url ?? ""}
+            trackPreview={element.track?.preview_url ?? "Best music"}
             trackId={element.track.artists[0].id}
           />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
